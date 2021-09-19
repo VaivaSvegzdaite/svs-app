f60291ed508414647fa593312d83555ce7f9bfd8
VaivaSvegzdaite <vaiva.svegzdaite@gmail.com>
2021-09-19 22:25:14 +0300

    added select option for size

--- app-ui/src/app/components/parcel-form/parcel-form.component.ts
+++ app-ui/src/app/components/parcel-form/parcel-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {Parcel, ParcelService} from "../../services/parcel.service";
 import {Receiver} from "../../services/receiver.service";
 import {Sender} from "../../services/sender.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-parcel-form',
@@ -11,13 +12,22 @@ import {Sender} from "../../services/sender.service";
 })
 export class ParcelFormComponent implements OnInit {
 
-  parcel: Parcel =  new Parcel(Number(''), '', Number(''),
+  parcel: Parcel = new Parcel(Number(''), '', Number(''),
     new Sender(Number(''), '', '', ''),
     new Receiver(Number(''), '', '', '', Number(''), ''));
+  parcelSizeForm!: FormGroup;
+  parcelSizes = ['S - siuntos dydis iki 0.5 kg',
+    'M - siuntos dydis iki 2 kg',
+    'L - siuntos dydis iki 9 kg',
+    'XL - siuntos dydis nuo 10 kg']
 
-  constructor(private parcelService: ParcelService, private router: Router) { }
+  constructor(private parcelService: ParcelService, private router: Router, private fb: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.parcelSizeForm = this.fb.group({
+      countryControl: ['Select']
+    });
   }
 
   createParcel(): void {
--- app-ui/src/app/components/parcel-update/parcel-update.component.ts
+++ app-ui/src/app/components/parcel-update/parcel-update.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Parcel, ParcelService} from "../../services/parcel.service";
 import {Sender} from "../../services/sender.service";
 import {Receiver} from "../../services/receiver.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-parcel-update',
@@ -13,11 +14,19 @@ export class ParcelUpdateComponent implements OnInit {
 
   id!: number;
   parcel!: Parcel;
+  parcelSizeForm!: FormGroup;
+  parcelSizes = ['S - siuntos dydis iki 0.5 kg',
+    'M - siuntos dydis iki 2 kg',
+    'L - siuntos dydis iki 9 kg',
+    'XL - siuntos dydis nuo 10 kg']
 
-  constructor(private parcelService: ParcelService, private route: ActivatedRoute, private router: Router) {
+  constructor(private parcelService: ParcelService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.parcelSizeForm = this.fb.group({
+      countryControl: ['Select']
+    });
     this.parcel = new Parcel(Number(''), '', Number(''),
       new Sender(Number(''), '', '', ''),
       new Receiver(Number(''), '', '', '', Number(''), ''));
